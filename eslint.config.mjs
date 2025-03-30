c5e1eba9c058760187553d716f59982c7b6b3e9b
Seheon Yu <49232918+seheon99@users.noreply.github.com>
2025-03-30 07:23:23 +0000

    configure ESLint with TypeScript and Tailwind CSS support; add pnpm workspace configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,50 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
 import { FlatCompat } from "@eslint/eslintrc";
+import js from "@eslint/js";
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
+const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
 });
 
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+// eslint-disable-next-line import/no-anonymous-default-export
+export default [...compat.extends(
+    "next/core-web-vitals",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:tailwindcss/recommended",
+    "prettier",
+), {
+    rules: {
+        "tailwindcss/enforces-negative-arbitrary-values": "off",
 
-export default eslintConfig;
+        "sort-imports": ["error", {
+            ignoreDeclarationSort: true,
+        }],
+
+        "import/order": ["error", {
+            groups: [
+                "builtin",
+                "external",
+                "internal",
+                "unknown",
+                "type",
+                ["parent", "index", "sibling", "object"],
+            ],
+
+            "newlines-between": "always",
+
+            alphabetize: {
+                order: "asc",
+                orderImportKind: "asc",
+                caseInsensitive: true,
+            },
+
+            warnOnUnassignedImports: true,
+        }],
+    },
+}];
