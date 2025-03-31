441872199ba21c92a45eae6c4095e9805bc3469b
Seheon Yu <49232918+seheon99@users.noreply.github.com>
2025-03-31 05:51:10 +0000

    add Prisma client and schema; update .gitignore and ESLint configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,44 +7,50 @@ import js from "@eslint/js";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
 });
 
 // eslint-disable-next-line import/no-anonymous-default-export
-export default [...compat.extends(
+export default [
+  ...compat.extends(
     "next/core-web-vitals",
     "plugin:@typescript-eslint/recommended",
-    "plugin:tailwindcss/recommended",
-    "prettier",
-), {
+    "prettier"
+  ),
+  {
     rules: {
-        "tailwindcss/enforces-negative-arbitrary-values": "off",
+      "sort-imports": [
+        "error",
+        {
+          ignoreDeclarationSort: true,
+        },
+      ],
 
-        "sort-imports": ["error", {
-            ignoreDeclarationSort: true,
-        }],
+      "import/order": [
+        "error",
+        {
+          groups: [
+            "builtin",
+            "external",
+            "internal",
+            "unknown",
+            "type",
+            ["parent", "index", "sibling", "object"],
+          ],
 
-        "import/order": ["error", {
-            groups: [
-                "builtin",
-                "external",
-                "internal",
-                "unknown",
-                "type",
-                ["parent", "index", "sibling", "object"],
-            ],
+          "newlines-between": "always",
 
-            "newlines-between": "always",
+          alphabetize: {
+            order: "asc",
+            orderImportKind: "asc",
+            caseInsensitive: true,
+          },
 
-            alphabetize: {
-                order: "asc",
-                orderImportKind: "asc",
-                caseInsensitive: true,
-            },
-
-            warnOnUnassignedImports: true,
-        }],
+          warnOnUnassignedImports: true,
+        },
+      ],
     },
-}];
+  },
+];
