951d1388daaad976f330505306b445bec6465880
Seheon Yu <49232918+seheon99@users.noreply.github.com>
2025-04-09 13:38:21 +0000

    feat: add Firebase configuration and authentication setup

---
+++ src/utilities/firebase.ts
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { browserSessionPersistence, getAuth, setPersistence } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyASUjmX8PrbH3CdauvB8PWRvQCYrB1KvdU",
+  authDomain: "green-signal-troop.firebaseapp.com",
+  projectId: "green-signal-troop",
+  storageBucket: "green-signal-troop.firebasestorage.app",
+  messagingSenderId: "630087341370",
+  appId: "1:630087341370:web:7b47e56822a1e7b425ac03",
+};
+
+export const firebaseApp = initializeApp(firebaseConfig);
+export const firebaseAuth = getAuth(firebaseApp);
+
+setPersistence(firebaseAuth, browserSessionPersistence);
--- src/utilities/index.ts
+++ src/utilities/index.ts
@@ -1,1 +1,3 @@
-export * from './generate-prng'
+export * from "./firebase";
+export * from "./generate-prng";
+export * from "./prisma-client";
