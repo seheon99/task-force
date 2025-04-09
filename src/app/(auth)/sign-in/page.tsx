44f914664698f5031ae444b33ab22ba62cc13973
Seheon Yu <49232918+seheon99@users.noreply.github.com>
2025-04-09 13:37:51 +0000

    feat(SignIn): implement sign-in functionality with Firebase authentication

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,3 +1,9 @@
+"use client";
+
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { useRouter } from "next/navigation";
+import { useCallback, useState } from "react";
+
 import {
   Button,
   Field,
@@ -8,24 +14,62 @@ import {
   Text,
   TextLink,
 } from "@/components/base";
+import { firebaseAuth } from "@/utilities";
+
+import type { FirebaseError } from "firebase/app";
 
 export default function SignInPage() {
+  const router = useRouter();
+
+  const [isLoading, setIsLoading] = useState(false);
+  const handleSubmit = useCallback(
+    async (email: string, password: string) => {
+      if (!isLoading) {
+        setIsLoading(true);
+        try {
+          const userCredential = await signInWithEmailAndPassword(
+            firebaseAuth,
+            email,
+            password
+          );
+          if (userCredential.user.uid) {
+            router.replace("/");
+          }
+        } catch (error) {
+          setIsLoading(false);
+          if ("code" in (error as FirebaseError)) {
+            if ((error as FirebaseError).code === "auth/invalid-credential") {
+              console.log(JSON.stringify(error));
+            }
+          }
+        }
+      }
+    },
+    [isLoading, setIsLoading, router]
+  );
+
   return (
     <form
       action="#"
-      method="POST"
       className="grid w-full max-w-sm grid-cols-1 gap-8"
+      onSubmit={async (e) => {
+        e.preventDefault();
+        const formData = new FormData(e.currentTarget);
+        const email = formData.get("email") as string;
+        const password = formData.get("password") as string;
+        handleSubmit(email, password);
+      }}
     >
       <Heading>로그인</Heading>
       <Field>
-        <Label>군번</Label>
+        <Label>이메일</Label>
         <Input type="email" name="email" />
       </Field>
       <Field>
         <Label>비밀번호</Label>
         <Input type="password" name="password" />
       </Field>
-      <Button type="submit" className="w-full">
+      <Button type="submit" className="w-full" disabled={isLoading}>
         로그인
       </Button>
       <Text>
