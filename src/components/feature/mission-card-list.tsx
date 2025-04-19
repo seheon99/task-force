03326262faa280a410fc23f4c00b777188981673
Seheon Yu <49232918+seheon99@users.noreply.github.com>
2025-04-19 13:17:50 +0000

    refactor: reorganize imports and enhance user experience in MissionCardList component

--- src/components/feature/mission-card-list.tsx
+++ src/components/feature/mission-card-list.tsx
@@ -1,25 +1,24 @@
 "use client";
 
-import { useMissions } from "@/hooks";
+import { TextLink } from "@/components/base";
+import { useMissions, useUser } from "@/hooks";
 
 import { MissionCard, MissionCreateButton } from "./";
 
 export function MissionCardList() {
-  const { data: missions, isLoading } = useMissions();
+  const { data: user } = useUser();
+  const { data: missions } = useMissions();
   return (
     <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
-      {isLoading && (
-        <div className="flex h-20 items-center justify-center text-gray-400">
-          Loading...
-        </div>
-      )}
-      {missions && (
+      {user ? (
         <>
           {missions?.map(({ id }) => (
             <MissionCard key={id} id={id} />
           ))}
           <MissionCreateButton />
         </>
+      ) : (
+        <TextLink href="/sign-in">로그인 하러 가기</TextLink>
       )}
     </div>
   );
