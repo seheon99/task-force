f048dc1d5649352c19f9b18e42c427618d7d04e6
Seheon Yu <49232918+seheon99@users.noreply.github.com>
2025-04-19 13:20:03 +0000

    refactor: remove experimental serverActions configuration from next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  experimental: {
-    serverActions: {
-      allowedOrigins: [
-        "localhost:3000",
-        "stunning-zebra-4w9wr4rwg7xhjv5p-3000.app.github.dev",
-      ],
-    },
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
