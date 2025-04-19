7b208c3f1503e9e9eee512b18bb469c7fd36ef23
Seheon Yu <49232918+seheon99@users.noreply.github.com>
2025-04-19 08:58:22 +0000

    feat: add getMissions function to retrieve missions for a user and update index exports

---
+++ src/actions/get-missions.ts
@@ -0,0 +1,17 @@
+import { prisma } from "@/utilities";
+import { User } from "@prisma";
+
+export async function getMissions({ uid }: { uid: User["id"] }) {
+  return await prisma.mission.findMany({
+    include: {
+      participants: true,
+    },
+    where: {
+      participants: {
+        some: {
+          userId: uid,
+        },
+      },
+    },
+  });
+}
--- src/actions/get-user.ts
+++ src/actions/get-user.ts
@@ -5,12 +5,5 @@ import { prisma } from "@/utilities";
 export async function getUser({ id }: { id: string }) {
   return await prisma.user.findUnique({
     where: { id },
-    include: {
-      participants: {
-        include: {
-          mission: true,
-        },
-      },
-    },
   });
 }
