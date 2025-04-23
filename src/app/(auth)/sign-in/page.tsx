7b23369b721927a8656950c717c111285377641b
Seheon Yu <49232918+seheon99@users.noreply.github.com>
2025-04-23 14:22:22 +0000

    feat: implement form handling with react-hook-form in SignInPage

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -7,9 +7,11 @@ import {
 } from "firebase/auth";
 import { useRouter } from "next/navigation";
 import { useCallback, useState } from "react";
+import { SubmitHandler, useForm } from "react-hook-form";
 
 import {
   Button,
+  ErrorMessage,
   Field,
   Heading,
   Input,
@@ -17,6 +19,7 @@ import {
   Strong,
   Text,
   TextLink,
+  toast,
 } from "@/components/base";
 import { firebaseAuth } from "@/utilities";
 
@@ -26,8 +29,8 @@ export default function SignInPage() {
   const router = useRouter();
 
   const [isLoading, setIsLoading] = useState(false);
-  const handleSubmit = useCallback(
-    async (email: string, password: string) => {
+  const onSubmit = useCallback<SubmitHandler<Inputs>>(
+    async ({ email, password }) => {
       if (!isLoading) {
         setIsLoading(true);
         try {
@@ -38,13 +41,21 @@ export default function SignInPage() {
             password
           );
           if (userCredential.user.uid) {
+            toast.success({
+              title: "로그인 성공",
+              description: `${userCredential.user.displayName}님 환영합니다`,
+            });
             router.replace("/");
           }
         } catch (error) {
           setIsLoading(false);
           if ("code" in (error as FirebaseError)) {
             if ((error as FirebaseError).code === "auth/invalid-credential") {
-              console.log(JSON.stringify(error));
+              toast.error({
+                title: "로그인 실패",
+                description:
+                  "가입하지 않은 아이디이거나 잘못된 비밀번호입니다.",
+              });
             }
           }
         }
@@ -53,26 +64,38 @@ export default function SignInPage() {
     [isLoading, setIsLoading, router]
   );
 
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Inputs>();
+
   return (
     <form
       action="#"
       className="grid w-full max-w-sm grid-cols-1 gap-8"
-      onSubmit={async (e) => {
-        e.preventDefault();
-        const formData = new FormData(e.currentTarget);
-        const email = formData.get("email") as string;
-        const password = formData.get("password") as string;
-        handleSubmit(email, password);
-      }}
+      onSubmit={handleSubmit(onSubmit)}
     >
       <Heading>로그인</Heading>
       <Field>
         <Label>이메일</Label>
-        <Input type="email" name="email" />
+        <Input
+          type="email"
+          invalid={!!errors.email}
+          {...register("email", { required: "이메일을 입력해주세요" })}
+        />
+        {errors.email && <ErrorMessage>{errors.email.message}</ErrorMessage>}
       </Field>
       <Field>
         <Label>비밀번호</Label>
-        <Input type="password" name="password" />
+        <Input
+          type="password"
+          invalid={!!errors.password}
+          {...register("password", { required: "비밀번호를 입력해주세요" })}
+        />
+        {errors.password && (
+          <ErrorMessage>{errors.password.message}</ErrorMessage>
+        )}
       </Field>
       <Button type="submit" className="w-full" disabled={isLoading}>
         로그인
@@ -86,3 +109,8 @@ export default function SignInPage() {
     </form>
   );
 }
+
+type Inputs = {
+  email: string;
+  password: string;
+};
