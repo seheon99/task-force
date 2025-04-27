80e4edd122a75e54f350c3764a091c3322763418
Seheon Yu <49232918+seheon99@users.noreply.github.com>
2025-04-27 14:06:32 +0000

    feat: implement ProfileSettings component with forms for personal info, password change, and account deletion

--- src/components/feature/index.ts
+++ src/components/feature/index.ts
@@ -1,5 +1,7 @@
 export * from "./mission-card-list";
 export * from "./mission-create-dialog";
+export * from "./settings";
+
 export * from "./user-dropdown";
 export * from "./vote-button";
 export * from "./vote-dialog";
