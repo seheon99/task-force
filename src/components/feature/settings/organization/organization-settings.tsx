44d17cfebd2e275ca454085c3d4816590df3ac70
Seheon Yu <49232918+seheon99@users.noreply.github.com>
2025-04-29 14:35:21 +0000

    fix: correct typo in expulsion success message for organization member

--- src/components/feature/settings/organization/organization-settings.tsx
+++ src/components/feature/settings/organization/organization-settings.tsx
@@ -115,7 +115,7 @@ export function OrganizationSettings({ id }: { id: Organization["id"] }) {
           await deleteMember({ id: member.id });
           toast.success({
             title: "내보내기 성공",
-            description: `${member.user.name}은 더이상 ${organization?.name}의 맴버가 아닙니다.`,
+            description: `${member.user.name}은 더이상 ${organization?.name}의 멤버가 아닙니다.`,
           });
           setDeletionMember(null);
         } catch (error) {
