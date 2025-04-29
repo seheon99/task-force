394fa09a199e91991029203287b527cccc2e68e4
Seheon Yu <49232918+seheon99@users.noreply.github.com>
2025-04-29 14:31:15 +0000

    feat: enhance UsersCombobox with disabled state handling and update user description

--- src/components/feature/users-combobox.tsx
+++ src/components/feature/users-combobox.tsx
@@ -8,21 +8,27 @@ import {
 } from "@/components/base";
 import { useUsers } from "@/swr";
 
-export function UsersCombobox({}: Omit<
-  React.ComponentPropsWithoutRef<typeof Combobox>,
+import type { User } from "@prisma";
+
+export function UsersCombobox({
+  disabled,
+  ...props
+}: Omit<
+  React.ComponentPropsWithoutRef<typeof Combobox<User>>,
   "options" | "displayValue" | "children"
 >) {
   const { data: users, isLoading: isLoadingUsers } = useUsers();
   return (
     <Combobox
-      disabled={isLoadingUsers}
+      {...props}
+      disabled={disabled || isLoadingUsers}
       options={users ?? []}
       displayValue={(user) => user?.name}
     >
       {(user) => (
         <ComboboxOption value={user}>
           <ComboboxLabel>{user.name}</ComboboxLabel>
-          <ComboboxDescription>{user.soldierId}</ComboboxDescription>
+          <ComboboxDescription>{user.unit}</ComboboxDescription>
         </ComboboxOption>
       )}
     </Combobox>
