969fc119f2570f38137104ee19e0b5b506340432
Seheon Yu <49232918+seheon99@users.noreply.github.com>
2025-05-01 14:03:59 +0000

    feature: restructure MissionCardList to include MissionFilter and filter missions by selected organization

--- src/components/feature/mission-card-list/mission-card-list.tsx
+++ src/components/feature/mission-card-list/mission-card-list.tsx
@@ -1,18 +1,28 @@
 "use client";
 
+import { useMissionFilterStore } from "@/stores";
 import { useMissions } from "@/swr";
 
 import { MissionCard } from "./mission-card";
 import { MissionCreateButton } from "./mission-create-button";
+import { MissionFilter } from "./mission-filter";
 
 export function MissionCardList() {
   const { data: missions } = useMissions();
+  const { selectedOrganizationId } = useMissionFilterStore();
   return (
-    <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
-      {missions?.map(({ id }) => (
-        <MissionCard key={id} id={id} />
-      ))}
-      <MissionCreateButton />
+    <div className="flex flex-col gap-4">
+      <MissionFilter />
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
+        {missions
+          ?.filter((m) =>
+            selectedOrganizationId
+              ? m.organizationId === selectedOrganizationId
+              : true,
+          )
+          .map(({ id }) => <MissionCard key={id} id={id} />)}
+        <MissionCreateButton />
+      </div>
     </div>
   );
 }
