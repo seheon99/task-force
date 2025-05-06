989b843a0c40ae4496ac33e0baa545212c31a829
Seheon Yu <49232918+seheon99@users.noreply.github.com>
2025-05-06 13:39:32 +0000

    feat: Simplify Home component by removing user authentication check

--- src/app/(stacked)/dashboard/page.tsx
+++ src/app/(stacked)/dashboard/page.tsx
@@ -1,14 +1,5 @@
-"use client";
-
-import { TextLink } from "@/components/base";
 import { MissionCardList } from "@/components/feature";
-import { useUser } from "@/swr";
 
 export default function Home() {
-  const { data: user } = useUser();
-  return user ? (
-    <MissionCardList />
-  ) : (
-    <TextLink href="/sign-in">로그인 하러 가기</TextLink>
-  );
+  return <MissionCardList />;
 }
