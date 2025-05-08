ed0b82362f08ffbc7cfe28685a7c66df98813c00
Seheon Yu <49232918+seheon99@users.noreply.github.com>
2025-05-08 10:49:53 +0000

    fix: Update import statements to use "server-only" in protected action generator and session verification

--- src/utilities/server-only/protected-action-generator.ts
+++ src/utilities/server-only/protected-action-generator.ts
@@ -1,4 +1,4 @@
-"use server";
+import "server-only";
 
 import { verifySession } from "@/utilities/server-only";
 
--- src/utilities/server-only/verify-session.ts
+++ src/utilities/server-only/verify-session.ts
@@ -1,3 +1,5 @@
+import "server-only";
+
 import { getUser } from "@/actions/database";
 import { verifySession as verifySessionAndReturnUserId } from "@/utilities/edge-only";
 
