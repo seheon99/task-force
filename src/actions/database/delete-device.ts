55439306d83c5296443f146f54c737456ca32b6f
Seheon Yu <49232918+seheon99@users.noreply.github.com>
2025-06-08 06:56:11 +0000

    feat: enhance deleteDevice action to support single device deletion by ID

--- src/actions/database/delete-device.ts
+++ src/actions/database/delete-device.ts
@@ -4,9 +4,16 @@ import { createProtection, prisma } from "@/utilities/server-only";
 
 import { User } from "@prisma";
 
-const _deleteDevice = createProtection(async (user: User) => {
-  return await prisma.device.deleteMany({ where: { userId: user.id } });
-});
+const _deleteDevice = createProtection(
+  async (user: User, props?: { id?: string }) => {
+    if (props?.id) {
+      const { id } = props;
+      return await prisma.device.delete({ where: { id } });
+    } else {
+      return await prisma.device.deleteMany({ where: { userId: user.id } });
+    }
+  },
+);
 
 export async function deleteDevice(...args: Parameters<typeof _deleteDevice>) {
   return await _deleteDevice(...args);
