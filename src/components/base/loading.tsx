3cdb1998d29430d8b4792d8eb5253252fd7ddcc0
Seheon Yu <49232918+seheon99@users.noreply.github.com>
2025-06-08 06:51:28 +0000

    feat: update Loading component to use CogIcon and accept className prop for styling

--- src/components/base/loading.tsx
+++ src/components/base/loading.tsx
@@ -1,3 +1,7 @@
-export function Loading() {
-  return <div>Loading</div>;
+import { CogIcon } from "@heroicons/react/16/solid";
+
+import { twcn } from "@/utilities";
+
+export function Loading({className}:{className?:string}) {
+  return <CogIcon className={twcn("size-6 animate-spin",className)} />;
 }
