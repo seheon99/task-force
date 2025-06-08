3394d0b0a544bde188b812ce1517042a37e2ff10
Seheon Yu <49232918+seheon99@users.noreply.github.com>
2025-06-08 04:01:07 +0000

    refactor: restructure device creation logic with protection and user context

--- src/actions/database/create-device.ts
+++ src/actions/database/create-device.ts
@@ -1,12 +1,29 @@
 "use server";
 
-import { prisma } from "@/utilities/server-only";
+import { createProtection, prisma } from "@/utilities/server-only";
 
-export async function registDevice(data: {
-  userId: string;
-  endpoint: string;
-  auth: string;
-  p256dh: string;
-}) {
-  return await prisma.device.create({ data });
+import { User } from "@prisma";
+
+const _createDevice = createProtection(
+  async (
+    user: User,
+    {
+      endpoint,
+      auth,
+      p256dh,
+    }: { endpoint: string; auth: string; p256dh: string },
+  ) => {
+    return await prisma.device.create({
+      data: {
+        userId: user.id,
+        endpoint,
+        auth,
+        p256dh,
+      },
+    });
+  },
+);
+
+export async function createDevice(...args: Parameters<typeof _createDevice>) {
+  return await _createDevice(...args);
 }
