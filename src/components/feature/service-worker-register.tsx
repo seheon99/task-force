a8aad7b0885753aab77907fd9a6132c0b41a466c
Seheon Yu <49232918+seheon99@users.noreply.github.com>
2025-06-08 06:50:33 +0000

    refactor: simplify service worker registration logic and remove subscription toggle button

--- src/components/feature/service-worker-register.tsx
+++ src/components/feature/service-worker-register.tsx
@@ -1,40 +1,31 @@
 "use client";
 
-import { useCallback, useEffect, useState } from "react";
+import { useEffect } from "react";
 
-import { Button } from "@/components/base";
+import { toast } from "@/components/base";
 import { vapidKeys } from "@/utilities/client-only";
 
 export function ServiceWorkerRegister() {
-  const [buttonText, setButtonText] = useState<"구독하기" | "구독 해지하기">(
-    "구독하기",
-  );
-
-  const toggleSubscription = useCallback(async () => {
-    const registration = await navigator.serviceWorker.ready;
-    const subscription = await registration.pushManager.getSubscription();
-    if (subscription) {
-      subscription.unsubscribe();
-      setButtonText("구독하기");
-    } else {
-      const subscription = await registration.pushManager.subscribe({
-        userVisibleOnly: true,
-        applicationServerKey: vapidKeys.publicKey,
-      });
-      console.log("subscription = ", subscription.toJSON());
-      setButtonText("구독 해지하기");
-    }
-  }, []);
-
   useEffect(() => {
     async function registServiceWorker() {
       const workerFile = "/service-worker.js";
       const registration = await navigator.serviceWorker.register(workerFile);
       const subscription = await registration.pushManager.getSubscription();
-      setButtonText(subscription ? "구독 해지하기" : "구독하기");
+      if (!subscription) {
+        const subscription = await registration.pushManager.subscribe({
+          userVisibleOnly: true,
+          applicationServerKey: vapidKeys.publicKey,
+        });
+        if (!subscription) {
+          toast.info({
+            title: "푸시 알람 해제",
+            description: "설정에서 언제든지 변경할 수 있습니다",
+          });
+        }
+      }
     }
     registServiceWorker();
   }, []);
 
-  return <Button onClick={toggleSubscription}>{buttonText}</Button>;
+  return <></>;
 }
