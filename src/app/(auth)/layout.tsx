5f358c3af635a1287dd392ecc488416aab33ebf7
Seheon Yu <49232918+seheon99@users.noreply.github.com>
2025-06-21 13:14:47 +0000

    refactor: remove unused authentication pages and related components

--- src/app/(auth)/layout.tsx
+++
@@ -1,9 +0,0 @@
-import { AuthLayout } from "@/components/base";
-
-export default function LoginLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
-  return <AuthLayout>{children}</AuthLayout>;
-}
