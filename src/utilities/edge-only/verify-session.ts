0d152bd5cb815095d6a61f0289cfca5240434861
Seheon Yu <49232918+seheon99@users.noreply.github.com>
2025-06-21 13:18:54 +0000

    refactor: simplify verifySession function by removing unused crypto key logic

--- src/utilities/edge-only/verify-session.ts
+++ src/utilities/edge-only/verify-session.ts
@@ -1,16 +1,16 @@
-import { decodeProtectedHeader, jwtVerify } from "jose";
+import { jwtVerify } from "jose";
 import { cookies } from "next/headers";
 
 import { ACCESS_TOKEN } from "@/constants";
-import { googlePublicKey } from "@/utilities/edge-only";
+
+import { environments } from "../server-only";
 
 export async function verifySession() {
   try {
     const token = await getAccessToken();
-    const cryptoKey = await getCryptoKeyFromToken(token);
-    const { payload } = await jwtVerify(token, cryptoKey);
-    const id = payload["user_id"] as string;
-    return id;
+    const secret = new TextEncoder().encode(environments.JWT_SECRET);
+    const { payload } = await jwtVerify(token, secret);
+    return payload.sub;
   } catch {
     return null;
   }
@@ -21,12 +21,4 @@ async function getAccessToken(): Promise<string> {
   const token = cookieStore.get(ACCESS_TOKEN)?.value;
   if (!token) throw new Error("Missing access token");
   return token;
-}
-
-async function getCryptoKeyFromToken(token: string): Promise<CryptoKey> {
-  const { kid } = decodeProtectedHeader(token);
-  if (!kid) throw new Error("Missing KID in token");
-  const cryptoKey = await googlePublicKey(kid);
-  if (!cryptoKey) throw new Error("Invalid KID or missing key");
-  return cryptoKey;
 }
