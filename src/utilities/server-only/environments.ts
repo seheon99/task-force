313d2fa2fd2898850ffa9dbbc3100344f60e3fea
Seheon Yu <49232918+seheon99@users.noreply.github.com>
2025-06-21 13:17:59 +0000

    feat: add JWT_SECRET to environments configuration

--- src/utilities/server-only/environments.ts
+++ src/utilities/server-only/environments.ts
@@ -4,5 +4,6 @@ export const environments = {
   VAPID_SUBJECT: process.env.VAPID_SUBJECT!,
   VAPID_PUBLIC_KEY: process.env.VAPID_PUBLIC_KEY!,
   VAPID_PRIVATE_KEY: process.env.VAPID_PRIVATE_KEY!,
+  JWT_SECRET: process.env.JWT_SECRET!,
   DB_ACCESS_LOG_FILENAME: process.env.DB_ACCESS_LOG_FILENAME!,
 };
