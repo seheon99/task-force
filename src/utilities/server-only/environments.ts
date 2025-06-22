ef97b47749d3eba9b8d2b7e30ae02e8a24be0b8e
Seheon Yu <49232918+seheon99@users.noreply.github.com>
2025-06-22 09:58:14 +0000

    refactor: add JWT_ISSUER to environments configuration

--- src/utilities/server-only/environments.ts
+++ src/utilities/server-only/environments.ts
@@ -5,5 +5,6 @@ export const environments = {
   VAPID_PUBLIC_KEY: process.env.VAPID_PUBLIC_KEY!,
   VAPID_PRIVATE_KEY: process.env.VAPID_PRIVATE_KEY!,
   JWT_SECRET: process.env.JWT_SECRET!,
+  JWT_ISSUER: process.env.JWT_ISSUER!,
   DB_ACCESS_LOG_FILENAME: process.env.DB_ACCESS_LOG_FILENAME!,
 };
